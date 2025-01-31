64bb274945237116a8706f6b82e2cf7ed037af26
Raptor English <rmenglis@usc.edu>
2025-01-31 05:21:02 +0000

    updated split code

--- split.cpp
+++ split.cpp
@@ -4,20 +4,54 @@ CSCI 104: Homework 1 Problem 1
 Write a recursive function to split a sorted singly-linked
 list into two sorted linked lists, where one has the even 
 numbers and the other contains the odd numbers. Students 
-will receive no credit for non-recursive solutions. 
+will receive no credit  non-recursive solutions. 
 To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <iostream>
 
-/* Add a prototype for a helper function here if you need */
+/* Add a prototype  a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  // base case to return if in is null
+  if (in == nullptr) {
+    return;
+  }
+
+  // take out first node of in and set next to nullptr to reset it
+  Node* curr_val = in;
+  in = in->next;
+
+  // reset nullptr so it can point at evens or odds
+  curr_val->next = nullptr;
+
+  if (curr_val->value % 2 == 1) {  // odd val
+
+    if (odds == nullptr) {  // edge case to handle initial null
+      odds = curr_val;
+    } else {  // update curr_val to point to curr odds
+      curr_val->next = odds;
+      odds = curr_val;
+    }
+  } else { // even val
+
+    if (evens == nullptr) {  // handle initial even null value
+      evens = curr_val;
+    } else {  // insert node into evens list
+      curr_val->next = evens;
+      evens = curr_val;
+    }
+  }
+
+  // call recursive function with new in
+  return split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -11,7 +11,33 @@ g++ split.cpp test_split.cpp -o test_split
 
 #include "split.h"
 
+#include <iostream>
+
 int main(int argc, char* argv[])
 {
+
+  // create nodes
+  Node* node1 = new Node(1, nullptr);
+  Node* node2 = new Node(2, node1);
+  Node* node3 = new Node(3, node2);
+  Node* node4 = new Node(4, node3);
+  Node* node5 = new Node(5, node4);
+  Node* node6 = new Node(6, node5);
+  Node* node7 = new Node(7, node6);
+
+  // initialize even and odd arrays
+  Node* evens = nullptr;
+  Node* odds = nullptr;
+
+  split(node7, odds, evens);
+  std::cout <<"hi"<<std::endl;
+
+  delete node1;
+  delete node2;
+  delete node3;
+  delete node4;
+  delete node5;
+  delete node6;
+  delete node7;
 
 }
